9d5dff902bfaa112c598867fdf5292bc7493aa38
Luis Alfredo Vejo Mendoza <2205483820101@ingenieria.usac.edu.gt>
2021-09-14 10:21:06 -0600

    Update P3.asm

--- P3.asm
+++ P3.asm
@@ -20,9 +20,16 @@ endm
 
 listaJugadores db
 listaPunteo dw 
+msg1 db 10,13, "|-----Luis Vejo - 201212527 - Practica No.3-----|$"
+msg2 db 10,13, "|-------------Juego de Mesa 'Damas'-------------|$"
+msg3 db 10,13, "Ingrese nombre para el Jugador 1$"
+msg4 db 10,13, "Ingrese nombre para el jugador 2$"
 
 .code
 begin proc far
+
+imprimir msg1
+imprimir msg2
 
 begin endp
 end
